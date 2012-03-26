4f872631a7de30644ace7e85ab63ba0fb7dc1482
Juan Ramón <juanramon.diaz@gmail.com>
2012-03-26 20:59:42 +0200

    init subscribe plugin

---
+++ subscribe/index.php
@@ -0,0 +1,144 @@
+<?php
+/*
+Plugin Name: Subscribe
+Plugin URI: http://www.osclass.org/
+Description: -
+Version: 0.9
+Author: OSClass
+Author URI: http://www.osclass.org/
+Short Name: subscribe
+Plugin update URI: 
+*/
+
+    define('SUBSCRIBE_VERSION', '0.9') ;
+    define('SUBSCRIBE_TABLE', DB_TABLE_PREFIX . 't_subscribers') ;
+
+    function subscribe_install() {
+        $dbCommand = get_dbCommand() ;
+
+        $file = osc_plugin_resource('subscribe/struct.sql') ;
+        if( !$dbCommand->importSQL($file) ) {
+            throw new Exception( $this->dao->getErrorLevel() . ' - ' . $this->dao->getErrorDesc() ) ;
+        }
+    }
+    osc_register_plugin(osc_plugin_path(__FILE__), 'subscribe_install') ;
+
+    /**
+     * url: osc_ajax_hook_url('subscribe')
+     * params: email
+     * return: 
+     *     json.error (true or false)
+     *     json.msg (String with a message)
+     * 
+     * example:
+     *     $.getJSON("<?php echo osc_ajax_hook_url('subscribe') ; ?>", { email: $('input[name="subscribe_email"]').val() }, function(json) {
+     *         console.log('JSON error: ' + json.error) ;
+     *         console.log('JSON message: ' + json.msg) ;
+     *     }) ;
+     */
+    function subscribe_ajax_request() {
+        switch( $return = subscribe_email() ) {
+            case -1:
+                $result = array(
+                    'error' => true,
+                    'msg'   => __('Entered an invalid email', 'subscribe')
+                ) ;
+            break;
+            case 0:
+                $result = array(
+                    'error' => true,
+                    'msg'   => __("You're already subscribed", 'subscribe')
+                ) ;
+            break;
+            case 1:
+                $result = array(
+                    'error' => true,
+                    'msg'   => __('Subscribed correctly', 'subscribe')
+                ) ;
+            break;
+            default:
+                $result = array(
+                    'error' => true,
+                    'msg'   => __("Error subscribing", 'subscribe')
+                ) ;
+            break;
+        }
+
+        echo json_encode($result) ;
+        exit ;
+    }
+    // ajax request
+    osc_add_hook('ajax_subscribe', 'subscribe_ajax_request') ;
+
+    // execute this code only if it's not an AJAX request
+    if( !defined('IS_AJAX') ) {
+        /**
+         * Form example:
+         *     <form action="" method="post">
+         *         <fieldset>
+         *             <input type="text" name="email" value="" >
+         *             <input type="submit" name="subscribe" value="submit">
+         *         </fieldset>
+         *     </form>
+         * 
+         * @since 0.9
+         */
+        function subscribe_post() {
+            // check if the POST to the subscribe plugin has been done
+            if( Params::getParam('subscribe') != '' ) {
+                switch( $return = subscribe_email() ) {
+                    case -1:
+                        osc_add_flash_error_message( __('Entered an invalid email', 'subscribe') ) ;
+                    break;
+                    case 0:
+                        osc_add_flash_warning_message( __("You're already subscribed", 'subscribe') ) ;
+                    break;
+                    case 1:
+                        osc_add_flash_ok_message( __('Subscribed correctly', 'subscribe') ) ;
+                    break;
+                    default:
+                        osc_add_flash_warning_message( __("Error subscribing", 'subscribe') ) ;
+                    break;
+                }
+            }
+        }
+        osc_add_hook('init', 'subscribe_post') ;
+    }
+
+    /**
+     * Insert email to be subscribed to the database
+     * 
+     * @since 0.9
+     * @return boolean true on success, false if the email is not inserted in the database
+     */
+    function subscribe_email() {
+        if( !osc_validate_email(Params::getParam('email')) ) {
+            return -1 ;
+        }
+
+        $dbCommand = get_dbCommand() ;
+        if( $dbCommand->insert(SUBSCRIBE_TABLE, array('s_email' => Params::getParam('email'))) ) {
+            return 1 ;
+        }
+
+        if( $dbCommand->getErrorLevel() == 1062 ) {
+            return 0 ;
+        }
+
+        return $dbCommand->getErrorLevel() ;
+    }
+
+    /**
+     * Get DBCommandClass object
+     * 
+     * @since 0.9
+     * @return DBCommandClass
+     */
+    function get_dbCommand() {
+        $conn = DBConnectionClass::newInstance() ;
+        $data = $conn->getOsclassDb();
+        return new DBCommandClass($data) ;
+    }
+
+    /* file end: subscribe/index.php */
+?>
