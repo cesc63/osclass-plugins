367bc77d189406bb3369961943838b22b6159a66
Juan Ramón <juanramon.diaz@gmail.com>
2012-05-21 23:33:33 +0200

    New feature: unsubscribe by e-mail; version 1.0

--- subscribe/index.php
+++ subscribe/index.php
@@ -3,25 +3,25 @@
 Plugin Name: Subscribe
 Plugin URI: http://www.osclass.org/
 Description: -
-Version: 0.9
+Version: 1.0
 Author: OSClass
 Author URI: http://www.osclass.org/
 Short Name: subscribe
 Plugin update URI: 
 */
 
-    define('SUBSCRIBE_VERSION', '0.9') ;
-    define('SUBSCRIBE_TABLE', DB_TABLE_PREFIX . 't_subscribers') ;
+    define('SUBSCRIBE_VERSION', '1.0');
+    define('SUBSCRIBE_TABLE', DB_TABLE_PREFIX . 't_subscribers');
 
     function subscribe_install() {
-        $dbCommand = get_dbCommand() ;
+        $dbCommand = get_dbCommand();
 
-        $sql = file_get_contents( osc_plugin_resource('subscribe/private/struct.sql') ) ;
+        $sql = file_get_contents( osc_plugin_resource('subscribe/private/struct.sql') );
         if( !$dbCommand->importSQL($sql) ) {
-            throw new Exception( $dbCommand->getErrorLevel() . ' - ' . $dbCommand->getErrorDesc() ) ;
+            throw new Exception( $dbCommand->getErrorLevel() . ' - ' . $dbCommand->getErrorDesc() );
         }
     }
-    osc_register_plugin(osc_plugin_path(__FILE__), 'subscribe_install') ;
+    osc_register_plugin(osc_plugin_path(__FILE__), 'subscribe_install');
 
     /**
      * url: osc_ajax_hook_url('subscribe')
@@ -31,9 +31,9 @@ Plugin update URI:
      *     json.msg (String with a message)
      * 
      * example:
-     *     $.getJSON("<?php echo osc_ajax_hook_url('subscribe') ; ?>", { email: $('input[name="subscribe_email"]').val() }, function(json) {
-     *         console.log('JSON error: ' + json.error) ;
-     *         console.log('JSON message: ' + json.msg) ;
+     *     $.getJSON("<?php echo osc_ajax_hook_url('subscribe'); ?>", { email: $('input[name="subscribe_email"]').val() }, function(json) {
+     *         console.log('JSON error: ' + json.error);
+     *         console.log('JSON message: ' + json.msg);
      *     }) ;
      */
     function subscribe_ajax_request() {
@@ -42,33 +42,33 @@ Plugin update URI:
                 $result = array(
                     'error' => true,
                     'msg'   => __('Entered an invalid email', 'subscribe')
-                ) ;
+                );
             break;
             case 0:
                 $result = array(
                     'error' => true,
                     'msg'   => __("You're already subscribed", 'subscribe')
-                ) ;
+                );
             break;
             case 1:
                 $result = array(
                     'error' => true,
                     'msg'   => __('Subscribed correctly', 'subscribe')
-                ) ;
+                );
             break;
             default:
                 $result = array(
                     'error' => true,
                     'msg'   => __("Error subscribing", 'subscribe')
-                ) ;
+                );
             break;
         }
 
-        echo json_encode($result) ;
-        exit ;
+        echo json_encode($result);
+        exit;
     }
     // ajax request
-    osc_add_hook('ajax_subscribe', 'subscribe_ajax_request') ;
+    osc_add_hook('ajax_subscribe', 'subscribe_ajax_request');
 
     // execute this code only if it's not an AJAX request
     if( !defined('IS_AJAX') ) {
@@ -88,21 +88,58 @@ Plugin update URI:
             if( Params::getParam('subscribe') != '' ) {
                 switch( $return = subscribe_email() ) {
                     case -1:
-                        osc_add_flash_error_message( __('Entered an invalid email', 'subscribe') ) ;
+                        osc_add_flash_error_message( __('Entered an invalid email', 'subscribe') );
                     break;
                     case 0:
-                        osc_add_flash_warning_message( __("You're already subscribed", 'subscribe') ) ;
+                        osc_add_flash_warning_message( __("You're already subscribed", 'subscribe') );
                     break;
                     case 1:
-                        osc_add_flash_ok_message( __('Subscribed correctly', 'subscribe') ) ;
+                        osc_add_flash_ok_message( __('Subscribed correctly', 'subscribe') );
                     break;
                     default:
-                        osc_add_flash_warning_message( __("Error subscribing", 'subscribe') ) ;
+                        osc_add_flash_warning_message( __("Error subscribing", 'subscribe') );
                     break;
                 }
             }
         }
-        osc_add_hook('init', 'subscribe_post') ;
+        osc_add_hook('init', 'subscribe_post');
+
+        /**
+         * url: http://www.domain.tld/?action=unsubscribe&email={email}
+         * 
+         * @since 1.0
+         */
+        function unsubscribe_post() {
+            if( Params::getParam('action') != 'unsubscribe' ) {
+                return false;
+            }
+
+            require_once(LIB_PATH . 'osclass/helpers/hErrors.php');
+            $email = urldecode( Params::getParam('email') );
+
+            $subscriber = get_subscriber($email);
+
+            if( !$subscriber ) {
+                $title   = sprintf(__('Error &raquo; %s', 'subscribe'), osc_page_title());
+                $message = __("The email you're trying to unsubscribe doesn't exist.", 'subscribe');
+                osc_die($title, $message);
+            }
+
+            if( !$subscriber['b_active'] ) {
+                $title   = sprintf(__('Error &raquo; %s', 'subscribe'), osc_page_title());
+                $message = __("You're already unsubscribed", 'subscribe');
+                osc_die($title, $message);
+            }
+
+            $dbCommand = get_dbCommand();
+            $set = array('b_active' => false, 'd_unsubscribe' => date('Y-m-d'));
+            $dbCommand->update(SUBSCRIBE_TABLE, $set, array('s_email' => $email));
+
+            $title   = sprintf(__('Unsubscribed correctly &raquo; %s', 'subscribe'), osc_page_title());
+            $message = __("You have been successfully unsubscribed", 'subscribe');
+            osc_die($title, $message);
+        }
+        osc_add_hook('init', 'unsubscribe_post');
     }
 
     /**
@@ -112,38 +149,66 @@ Plugin update URI:
      * @return boolean true on success, false if the email is not inserted in the database
      */
     function subscribe_email() {
-        $email = urldecode( Params::getParam('email') ) ;
+        $email = urldecode( Params::getParam('email') );
         if( !osc_validate_email($email) ) {
-            return -1 ;
+            return -1;
         }
 
-        $dbCommand = get_dbCommand() ;
+        $dbCommand = get_dbCommand();
         $aInsert   = array(
             's_email'        => $email,
             'd_subscription' => date('Y-m-d'),
             'c_ip'           => $_SERVER['REMOTE_ADDR']
         ) ;
         if( $dbCommand->insert(SUBSCRIBE_TABLE, $aInsert) ) {
-            return 1 ;
+            return 1;
         }
 
         if( $dbCommand->getErrorLevel() == 1062 ) {
-            return 0 ;
+            $set = array('b_active' => true, 'd_unsubscribe' => '');
+            $result = $dbCommand->update(SUBSCRIBE_TABLE, $set, array('s_email' => $email));
+            return $result;
         }
 
-        return $dbCommand->getErrorLevel() ;
+        return $dbCommand->getErrorLevel();
     }
 
     /**
-     * Get DBCommandClass object
+     * Get subscriber if exists
      * 
-     * @since 0.9
-     * @return DBCommandClass
+     * @since 1.0
+     * @return mixed
      */
-    function get_dbCommand() {
-        $conn = DBConnectionClass::newInstance() ;
-        $data = $conn->getOsclassDb();
-        return new DBCommandClass($data) ;
+    function get_subscriber($email) {
+        $dbCommand = get_dbCommand();
+        $dbCommand->select();
+        $dbCommand->where('s_email', $email);
+        $dbCommand->limit(1);
+        $rs = $dbCommand->get(SUBSCRIBE_TABLE);
+
+        if( !$rs ) {
+            return false;
+        }
+
+        if( $rs->numRows() == 0 ) {
+            return false;
+        }
+
+        return $rs->row();
+    }
+
+    if( !function_exists('get_dbCommand') ) {
+        /**
+         * Get DBCommandClass object
+         * 
+         * @since 0.9
+         * @return DBCommandClass
+         */
+        function get_dbCommand() {
+            $conn = DBConnectionClass::newInstance();
+            $data = $conn->getOsclassDb();
+            return new DBCommandClass($data);
+        }
     }
 
     /* file end: subscribe/index.php */
--- subscribe/private/struct.sql
+++ subscribe/private/struct.sql
@@ -2,7 +2,8 @@ CREATE TABLE /*TABLE_PREFIX*/t_subscribers (
     pk_i_id INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
     s_email VARCHAR(100) NOT NULL,
     b_active TINYINT(1) NOT NULL DEFAULT 1,
-    d_subscription date default NULL,
+    d_subscription DATE DEFAULT NULL,
+    d_unsubscribe DATE DEFAULT NULL,
     c_ip char(64) NOT NULL default '0.0.0.0',
 
         PRIMARY KEY (pk_i_id),
