74f935156375349b1df2f364961be5b43c46dc2e
Carlos <garciademarina@gmail.com>
2012-08-07 11:54:02 +0200

    removed helper functions

--- market/item_detail.php
+++ market/item_detail.php
@@ -1,8 +1,9 @@
+<?php $market_aux = ModelMarket::newInstance()->getFileFromItem(osc_item_id()); ?>
 <script>
     $("#download_market").on("click", function() {
         $.getJSON(
         '<?php echo osc_base_url(true); ?>?page=ajax&action=custom&ajaxfile=<?php echo osc_plugin_folder(__FILE__) . 'ajax.php';?>&paction=download',
-            {"code" : '<?php echo market_slug()?>'},
+            {"code" : '<?php echo @$market_aux['s_slug']?>'},
             function(data){
                 if(data.error==0) {
                     $("#download_market").append(data.msg);
