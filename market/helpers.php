8c886d7ab8c6ddf7e3614a4cc48bf564ee82a07e
Carlos <garciademarina@gmail.com>
2012-08-07 12:48:54 +0200

    theme osclass_org needs this helpers

---
+++ market/helpers.php
@@ -0,0 +1,74 @@
+<?php
+
+    function market_ad() {
+        if(View::newInstance()->_exists("market_ad")) {
+            return View::newInstance()->_get("market_ad");
+        } else {
+            $market = ModelMarket::newInstance()->findByItemId(osc_item_id());
+            $market['files'][0] = ModelMarket::newInstance()->getFileFromItem(osc_item_id());
+            View::newInstance()->_exportVariableToView("market_ad", $market);
+            return $market;
+        }
+    }
+    
+    function market_id() {
+        $market = market_ad();
+        return @$market['pk_i_id'];
+    }
+
+    function market_slug() {
+        $market = market_ad();
+        return @$market['s_slug'];
+    }
+
+    function market_banner_url() {
+        $market = market_ad();
+        return @$market['s_banner'];
+    }
+
+    function market_preview_url() {
+        $market = market_ad();
+        return @$market['s_preview'];
+    }
+
+
+    function market_file() {
+        $market = market_ad();
+        if(isset($market['files'][0])) {
+            return $market['files'][0];
+        } else {
+            return array();
+        }
+    }
+    
+    function market_file_download() {
+        $file = market_file();
+        return @$file['s_download'];
+    }
+    
+    function market_file_version() {
+        $file = market_file();
+        return @$file['s_version'];
+    }
+    
+    function market_file_compatible_versions() {
+        $file = market_file();
+        return @$file['s_compatible'];
+    }
+    
+    function market_file_compatible_versions_show() {
+        $file = market_file();
+        return @$file['s_compatible_show'];
+    }
+    
+    function market_file_path() {
+        $file = market_file();
+        return @$file['s_file'];
+    }
+
+    function market_file_enabled() {
+        $file = market_file();
+        return @$file['b_enabled']==1?true:false;
+    }
+    
+?>
