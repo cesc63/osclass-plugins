491416736d0d4198c7ced731c602824bc4357797
conejoninja <conejo@theninjabunny.com>
2012-10-30 16:35:03 +0100

    small modifications

--- corporateboardmenu/admin/settings.php
+++ corporateboardmenu/admin/settings.php
@@ -3,55 +3,58 @@ $adminManager = Admin::newInstance();
 $admin = $adminManager->findByPrimaryKey(osc_logged_admin_id());
 ?>
 <div id="general-settings">
-    <form name="corporateboard_form" method="post">
+    <form name="corporateboardmenu_form" method="post">
+        <input type="hidden" name="page" value="plugins" />
+        <input type="hidden" name="action" value="renderplugin" />
+        <input type="hidden" name="file" value="<?php echo osc_plugin_folder(__FILE__); ?>settings.php" />
         <input type="hidden" name="subaction" value="update-settings">
         <fieldset>
             <div class="form-horizontal">
-                <h2 class="render-title"><?php _e('General Settings') ; ?></h2>
+                <h2 class="render-title"><?php _e('General Settings', 'corporateboardmenu') ; ?></h2>
                 <div class="form-row">
-                    <div class="form-label"><?php _e('Company name', 'corporateboard') ; ?></div>
+                    <div class="form-label"><?php _e('Company name', 'corporateboardmenu') ; ?></div>
                     <div class="form-controls"><input type="text" class="xlarge" name="pageTitle" value="<?php echo osc_esc_html( osc_page_title() ); ?>" /></div>
                 </div>
                 <div class="form-row">
-                    <div class="form-label"><?php _e('Contact email') ; ?></div>
+                    <div class="form-label"><?php _e('Contact email', 'corporateboardmenu') ; ?></div>
                     <div class="form-controls"><input type="text" class="large" name="contactEmail" value="<?php echo osc_esc_html( osc_contact_email() ) ; ?>" /></div></div>
-                <h2 class="render-title separate-top"><?php _e('Edit admin user') ; ?></h2>
+                <h2 class="render-title separate-top"><?php _e('Edit admin user', 'corporateboardmenu') ; ?></h2>
                 <div class="form-row clear">
-                    <div class="form-label"><?php _e('Name <em>(required)</em>') ; ?></div>
+                    <div class="form-label"><?php _e('Name <em>(required)</em>', 'corporateboardmenu') ; ?></div>
                     <div class="form-controls">
                         <?php AdminForm::name_text($admin) ; ?>
                     </div>
                 </div>
                 <div class="form-row">
-                    <div class="form-label"><?php _e('Username <em>(required)</em>') ; ?></div>
+                    <div class="form-label"><?php _e('Username <em>(required)</em>', 'corporateboardmenu') ; ?></div>
                     <div class="form-controls"><?php AdminForm::username_text($admin) ; ?></div>
                 </div>
                 <div class="form-row">
-                    <div class="form-label"><?php _e('Current password') ; ?></div>
+                    <div class="form-label"><?php _e('Current password', 'corporateboardmenu') ; ?></div>
                     <div class="form-controls">
                         <?php AdminForm::old_password_text($admin) ; ?>
-                        <p class="help-inline"><em><?php _e('If you want to change your password, type your current password here. Otherwise, leave this blank.') ; ?></em></p>
+                        <p class="help-inline"><em><?php _e('If you want to change your password, type your current password here. Otherwise, leave this blank.', 'corporateboardmenu') ; ?></em></p>
                     </div>
                 </div>
                 <div class="form-row">
-                    <div class="form-label"><?php _e('New password') ; ?></div>
+                    <div class="form-label"><?php _e('New password', 'corporateboardmenu') ; ?></div>
                     <div class="form-controls">
                         <?php AdminForm::password_text($admin) ; ?>
                     </div>
                     <div class="form-controls">
                         <?php AdminForm::check_password_text($admin) ; ?>
-                        <p class="help-inline"><em><?php _e('Type your new password again') ; ?></em></p>
+                        <p class="help-inline"><em><?php _e('Type your new password again', 'corporateboardmenu') ; ?></em></p>
                     </div>
                 </div>
-                <h2 class="render-title separate-top"><?php _e('Google analytics') ; ?></h2>
+                <h2 class="render-title separate-top"><?php _e('Google analytics', 'corporateboardmenu') ; ?></h2>
                 <div class="form-row clear">
-                    <div class="form-label"><?php _e('Tracking ID') ; ?></div>
+                    <div class="form-label"><?php _e('Tracking ID', 'corporateboardmenu') ; ?></div>
                     <div class="form-controls">
-                        <input type="text" class="xlarge" name="googleanalytics_trackingid" value="<?php echo osc_esc_html( osc_get_preference('googleanalytics_trackingid', 'corporateboard') ); ?>" />
+                        <input type="text" class="xlarge" name="googleanalytics_trackingid" value="<?php echo osc_esc_html( osc_get_preference('googleanalytics_trackingid', 'corporateboardmenu') ); ?>" />
                     </div>
                 </div>
                 <div class="form-actions">
-                    <input type="submit" id="save_changes" value="<?php echo osc_esc_html( __('Save changes', 'corporateboard') ) ; ?>" class="btn btn-submit" />
+                    <input type="submit" id="save_changes" value="<?php echo osc_esc_html( __('Save changes', 'corporateboardmenu') ) ; ?>" class="btn btn-submit" />
                 </div>
             </div>
         </fieldset>
--- corporateboardmenu/index.php
+++ corporateboardmenu/index.php
@@ -58,6 +58,13 @@ Plugin update URI:
             );
 
         osc_add_admin_menu_page(
+            __('Killer Questions', 'corporateboardmeun'),
+            osc_admin_render_plugin_url("jobboard/manage_killer.php"),
+            'killer_questions',
+            'moderator'
+        );
+
+        osc_add_admin_menu_page(
             __('Pages', 'corporateboardmenu'),
             osc_admin_base_url(true) .'?page=pages',
             'pages',
@@ -94,6 +101,112 @@ Plugin update URI:
                 'moderator'
                 );
 
+            osc_add_admin_submenu_page(
+                'settings',
+                __('Default locations', 'corporateboardmenu'),
+                osc_admin_render_plugin_url('jobboard/admin/settings.php'),
+                'settings_jobboard',
+                'moderator'
+                );
+
     }
     osc_add_hook('admin_header', 'corporateboardmenu_admin_menu', 6);
+
+
+    function corporateboardmenu_save_settings() {
+        $iUpdated     = 0;
+        $array        = array();
+        $adminManager = Admin::newInstance();
+        $aAdmin       = $adminManager->findByPrimaryKey(osc_logged_admin_id());
+        $conditions   = array('pk_i_id' => osc_logged_admin_id());
+
+        $sPassword    = Params::getParam('s_password', false, false);
+        $sPassword2   = Params::getParam('s_password2', false, false);
+        $sOldPassword = Params::getParam('old_password', false, false);
+        $sName        = Params::getParam('s_name');
+        $sEmail       = Params::getParam('contactEmail');
+        $sUserName    = Params::getParam('s_username');
+
+        // cleaning parameters
+        $sPassword   = strip_tags($sPassword);
+        $sPassword   = trim($sPassword);
+        $sPassword2  = strip_tags($sPassword2);
+        $sPassword2  = trim($sPassword2);
+        $sName       = strip_tags($sName);
+        $sName       = trim($sName);
+        $sEmail      = strip_tags($sEmail);
+        $sEmail      = trim($sEmail);
+        $sUserName   = strip_tags($sUserName);
+        $sUserName   = trim($sUserName);
+
+        // Checks for legit data
+        if( !osc_validate_email($sEmail, true) ) {
+            osc_add_flash_warning_message(__("Email invalid", 'corporateboardmenu'), 'admin');
+            redirect_to_url(osc_admin_render_plugin_url('corporateboardmenu/admin/settings.php'));
+        }
+
+        if( !osc_validate_username($sUserName) ) {
+            osc_add_flash_warning_message(__("Username invalid", 'corporateboardmenu'), 'admin');
+            redirect_to_url(osc_admin_render_plugin_url('corporateboardmenu/admin/settings.php'));
+        }
+
+        if( $sName == '' ) {
+            osc_add_flash_warning_message(__("Name invalid", 'corporateboardmenu'), 'admin');
+            redirect_to_url(osc_admin_render_plugin_url('corporateboardmenu/admin/settings.php'));
+        }
+
+        if( Params::getParam('pageTitle') == '' ) {
+            osc_add_flash_warning_message(__("Company name invalid", 'corporateboardmenu'), 'admin');
+            redirect_to_url(osc_admin_render_plugin_url('corporateboardmenu/admin/settings.php'));
+        }
+
+        if( $aAdmin['s_username'] != $sUserName ) {
+            if( $adminManager->findByUsername( $sUserName ) ) {
+                osc_add_flash_warning_message(__('Existing username', 'corporateboardmenu'), 'admin');
+                redirect_to_url(osc_admin_render_plugin_url('corporateboardmenu/admin/settings.php'));
+            }
+        }
+
+        if($sOldPassword != '' ) {
+            if( $sPassword=='' ) {
+                osc_add_flash_warning_message(__("Password invalid", 'corporateboardmenu'), 'admin');
+                redirect_to_url(osc_admin_render_plugin_url('corporateboardmenu/admin/settings.php'));
+            } else {
+                $firstCondition  = ( sha1($sOldPassword) == $aAdmin['s_password'] );
+                $secondCondition = ( $sPassword == $sPassword2 );
+                if( $firstCondition && $secondCondition ) {
+                    $array['s_password'] = sha1($sPassword);
+                } else {
+                    osc_add_flash_warning_message(__("The password couldn't be updated. Passwords don't match", 'corporateboardmenu'), 'admin');
+                    redirect_to_url(osc_admin_render_plugin_url('corporateboardmenu/admin/settings.php'));
+                }
+            }
+        }
+        // update preference
+        osc_set_preference('contactEmail', Params::getParam('contactEmail'));
+        osc_set_preference('pageTitle', Params::getParam('pageTitle'));
+        osc_set_preference('googleanalytics_trackingid', Params::getParam('googleanalytics_trackingid'), 'corporateboardmenu');
+        osc_reset_preferences();
+
+        $array['s_name']     = $sName; //Params::getParam('s_name');
+        $array['s_username'] = $sUserName;
+        $array['s_email']    = $sEmail;
+
+        $iUpdated = $adminManager->update($array, $conditions);
+
+        osc_add_flash_ok_message(__('The admin has been updated', 'corporateboardmenu'), 'admin');
+        redirect_to_url(osc_admin_render_plugin_url('corporateboardmenu/admin/settings.php'));
+    }
+
+    function corporateboardmenu_init() {
+        if(Params::getParam('page') == 'plugins' &&
+                Params::getParam('action') == 'renderplugin' &&
+                Params::getParam('file') == 'corporateboardmenu/admin/settings.php' &&
+                Params::getParam('subaction') == 'update-settings' ) {
+                    corporateboardmenu_save_settings();
+        }
+    }
+    osc_add_hook('init_admin', 'corporateboardmenu_init');
+
+
     /* file end: corporateboardmenu/index.php */
