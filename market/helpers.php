ecfd6e517fe43f30ae5f66301f0e10e10a582ea7
Carlos <garciademarina@gmail.com>
2013-02-04 18:00:44 +0100

    banner size function related, added function market_get_type

--- market/helpers.php
+++ market/helpers.php
@@ -10,7 +10,7 @@
             return $market;
         }
     }
-    
+
     function market_id() {
         $market = market_ad();
         return @$market['pk_i_id'];
@@ -24,6 +24,11 @@
     function market_banner_url() {
         $market = market_ad();
         return @$market['s_banner'];
+    }
+
+    function market_banner_path() {
+        $market = market_ad();
+        return @$market['s_banner_path'];
     }
 
     function market_preview_url() {
@@ -40,27 +45,27 @@
             return array();
         }
     }
-    
+
     function market_file_download() {
         $file = market_file();
         return @$file['s_download'];
     }
-    
+
     function market_file_version() {
         $file = market_file();
         return @$file['s_version'];
     }
-    
+
     function market_file_compatible_versions() {
         $file = market_file();
         return @$file['s_compatible'];
     }
-    
+
     function market_file_compatible_versions_show() {
         $file = market_file();
         return @$file['s_compatible_show'];
     }
-    
+
     function market_file_path() {
         $file = market_file();
         return @$file['s_file'];
@@ -70,5 +75,56 @@
         $file = market_file();
         return @$file['b_enabled']==1?true:false;
     }
-    
+
+    /**
+     * Return type {THEME/LANGUAGE/PLUGIN} depends on category id.
+     * @param type $categoryId
+     * @return type
+     */
+    function market_get_type($categoryId) {
+
+        $aCategoryPlugins       = explode(',', osc_get_preference('market_categories_plugins','market'));
+        $aCategoryThemes        = explode(',', osc_get_preference('market_categories_theme','market'));
+        $aCategoryLanguages     = explode(',', osc_get_preference('market_categories_languages','market'));
+
+        if( in_array($file['fk_i_category_id'], $aCategoryThemes) ) {
+            return 'THEME';
+        } else if( in_array($file['fk_i_category_id'], $aCategoryLanguages) ) {
+            return 'LANGUAGE';
+        } else {
+            return 'PLUGIN';
+        }
+    }
+
+    /**
+     * Return type widh and height for resize market banners depending on category id.
+     *
+     * res['w'] = width
+     * res['h'] = height
+     *
+     * @param type $categoryId
+     * @return type
+     */
+    function market_banner_size($categoryId) {
+
+        $aSizes = array(
+            'plugins'       => array('w' => 624, 'h' => 224),
+            'themes'        => array('w' => 400, 'h' => 400),
+            'languages'     => array('w' => 400, 'h' => 400)
+        );
+
+        $aCategoryPlugins       = explode(',', osc_get_preference('market_categories_plugins','market'));
+        $aCategoryThemes        = explode(',', osc_get_preference('market_categories_theme','market'));
+        $aCategoryLanguages     = explode(',', osc_get_preference('market_categories_languages','market'));
+
+        if( in_array($categoryId, $aCategoryThemes) ) {
+            $size = $aSizes['plugins'];
+        } else if( in_array($categoryId, $aCategoryLanguages) ) {
+            $size = $aSizes['themes'];
+        } else {
+            $size = $aSizes['languages'];
+        }
+        return $size;
+    }
+
 ?>
