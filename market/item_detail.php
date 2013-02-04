9c8117254f71e5c24b1a7030802d6f23462d263d
Carlos <garciademarina@gmail.com>
2013-02-04 17:57:48 +0100

    minor bug fixed

--- market/item_detail.php
+++ market/item_detail.php
@@ -2,7 +2,7 @@
 <script>
     $("#download_market").on("click", function() {
         $.getJSON(
-        '<?php echo osc_base_url(true); ?>?page=ajax&action=custom&ajaxfile=<?php echo osc_plugin_folder(__FILE__) . 'ajax.php';?>&paction=download&website=ocadmin_manualy',
+        '<?php echo osc_base_url(true); ?>?page=ajax&action=custom&ajaxfile=<?php echo osc_plugin_folder(__FILE__) . 'ajax.php';?>&paction=download',
             {"code" : '<?php echo @$market_aux['s_slug']?>'},
             function(data){
                 if(data.error==0) {
