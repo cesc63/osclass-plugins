c09c13b44774d3049b8980b022d8f09976e39138
Carlos <garciademarina@gmail.com>
2013-02-22 13:18:41 +0100

    removed s_download case, removed debug code

--- market/download.php
+++ market/download.php
@@ -6,8 +6,6 @@
 
     $code = Params::getParam('code') ;
 
-    error_log('Api market ... ');
-
     if( $code != '' ) {
 
         if( preg_match('|(.+)@([A-Za-z0-9\.-]+)$|', $code, $m) ) {
@@ -21,50 +19,27 @@
         $file = ModelMarket::newInstance()->getFileForDownloadBySlug($slug, $version) ;
         if( !empty($file) ) {
             $osclass_version = '';
-            error_log('User agent -  >  '.$_SERVER['HTTP_USER_AGENT']);
             if(isset($_SERVER['HTTP_USER_AGENT'])) {
                 if(preg_match("|Osclass \(v\.([0-9]+)\)|", $_SERVER['HTTP_USER_AGENT'], $match)) {
                     $osclass_version = $match[1];
-                    error_log('-> FROM oc-admin [' . $osclass_version . ']');
-                } else {
-                    error_log('FROM direct download');
                 }
             }
 
             // save market download stats
             ModelMarket::newInstance()->insertStat($file['fk_i_market_id'], $file['pk_i_id'], isset($_SERVER['REMOTE_HOST'])?$_SERVER['REMOTE_HOST']:'', isset($_SERVER['REMOTE_ADDR'])?$_SERVER['REMOTE_ADDR']:'', $osclass_version) ;
 
-            error_log("market::plugin send file to download " . $file['s_source_file']);
-
-            if($file['s_source_file']!='') {
-                error_log("market download s_source:file ... ". $file['s_source_file']);
-                header( 'Content-Description: File Transfer' ) ;
-                header( 'Content-Type: application/octet-stream' ) ;
-                header( 'Content-Disposition: attachment; filename=' . basename($file['s_source_file']) ) ;
-                header( 'Content-Transfer-Encoding: binary' ) ;
-                header( 'Expires: 0' ) ;
-                header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' ) ;
-                header( 'Pragma: public' ) ;
-                header( 'Content-Length: ' . filesize($file['s_source_file']) ) ;
-                @ob_clean() ;
-                flush() ;
-                readfile($file['s_source_file']) ;
-                exit ;
-            } else {
-                error_log("market download s_download... ". $file['s_download']);
-                header( 'Content-Description: File Transfer' ) ;
-                header( 'Content-Type: application/octet-stream' ) ;
-                header( 'Content-Disposition: attachment; filename=' . basename(str_replace("/download", "", $file['s_download'])) ) ;
-                header( 'Content-Transfer-Encoding: binary' ) ;
-                header( 'Expires: 0' ) ;
-                header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' ) ;
-                header( 'Pragma: public' ) ;
-                //header( 'Content-Length: ' . filesize($file['s_source_file']) ) ;
-                @ob_clean() ;
-                flush() ;
-                readfile($file['s_download']) ;
-                exit ;
-            }
+            header( 'Content-Description: File Transfer' ) ;
+            header( 'Content-Type: application/octet-stream' ) ;
+            header( 'Content-Disposition: attachment; filename=' . basename($file['s_source_file']) ) ;
+            header( 'Content-Transfer-Encoding: binary' ) ;
+            header( 'Expires: 0' ) ;
+            header( 'Cache-Control: must-revalidate, post-check=0, pre-check=0' ) ;
+            header( 'Pragma: public' ) ;
+            header( 'Content-Length: ' . filesize($file['s_source_file']) ) ;
+            @ob_clean() ;
+            flush() ;
+            readfile($file['s_source_file']) ;
+            exit ;
         }
     }
 
