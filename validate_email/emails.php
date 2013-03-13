0afe99ff1c76941a74f238fe0d98521e9eab3d99
Carlos <garciademarina@gmail.com>
2013-03-13 10:11:59 +0100

    deprecated split removed

--- validate_email/emails.php
+++ validate_email/emails.php
@@ -1,20 +1,20 @@
 <?php
 
-if( $_REQUEST['contactEmail'] != '' ) {
+if( array_key_exists($_REQUEST['contactEmail']) ) {
     $email = $_REQUEST['contactEmail'];
-} else if( $_REQUEST['yourEmail'] != '' ) {
+} else if( array_key_exists($_REQUEST['yourEmail']) ) {
     $email = $_REQUEST['yourEmail'];
-} else if( $_REQUEST['friendEmail'] != '' ) {
+} else if( array_key_exists($_REQUEST['friendEmail'] ) {
     $email = $_REQUEST['friendEmail'];
-} else if( $_REQUEST['new_email'] != '' ) {
+} else if( array_key_exists($_REQUEST['new_email'] ) {
     $email = $_REQUEST['new_email'];
-} else if( $_REQUEST['s_email'] != '' ) {
+} else if( array_key_exists($_REQUEST['s_email'])  ) {
     $email = $_REQUEST['s_email'];
 } else {
     echo "false";
 }
 
-list($user, $domain) = split('@', $email);
+list($user, $domain) = explode('@', $email);
 if( @checkdnsrr($domain, 'MX') ) {
     echo "true";
 } else {
